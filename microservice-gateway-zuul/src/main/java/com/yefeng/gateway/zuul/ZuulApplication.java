7805b380f26149419dce9871a9b3c382be30c7f3
刘小刚 <liuxiaogang.bj@fang.com>
2020-09-22 18:09:12 +0800

    ----

---
+++ microservice-gateway-zuul/src/main/java/com/yefeng/gateway/zuul/ZuulApplication.java
@@ -0,0 +1,22 @@
+package com.yefeng.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import java.util.TimeZone;
+
+/**
+ * @description:
+ * @author: liuxiaogang.bj
+ * @create: 2020/9/22 17:16
+ **/
+@SpringBootApplication
+@ServletComponentScan
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
