a378b60da9f9030b8cd9e3652f24e1812ae78a78
刘小刚 <liuxiaogang.bj@fang.com>
2020-09-25 09:01:43 +0800

    ----

--- microservice-gateway-zuul/src/main/java/com/yefeng/gateway/zuul/ZuulApplication.java
+++ microservice-gateway-zuul/src/main/java/com/yefeng/gateway/zuul/ZuulApplication.java
@@ -3,6 +3,7 @@ package com.yefeng.gateway.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import java.util.TimeZone;
 
@@ -13,6 +14,7 @@ import java.util.TimeZone;
  **/
 @SpringBootApplication
 @ServletComponentScan
+@EnableZuulProxy
 public class ZuulApplication {
 
 	public static void main(String[] args) {
